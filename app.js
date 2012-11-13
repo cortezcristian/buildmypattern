c38c6a4d816e845e1e61a813f387991b3a4e13fd
Cristian <cortez.cristian@gmail.com>
2012-11-13 20:24:47 -0300

    Adding favicon and basic config

--- app.js
+++ app.js
@@ -1,21 +1,52 @@
 
 /**
- * Module dependencies.
+ * Module dependencies. 
  */
 
 var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
-
+  , path = require('path')
+  // Global Configurations
+  , mainDomain = "buildmypattern.jit.su"
+  , mainPort = 80
+  , dbDomain = 'ds039257.mongolab.com:39257'
+  , dbName = 'nodejitsu_cortezcristian_nodejitsudb5110793134'
+  , dbUser = 'nodejitsu_cortezcristian'
+  , dbPass = '7hhta9aok10tc0raqrqbei8jmt';
+  
+switch(process.env.NODE_ENV){
+	case 'development':
+		mainDomain = "127.0.0.1";
+		mainPort = 8000;
+		dbDomain = 'localhost';
+		dbName = 'buildmypattern_dev';
+		dbUser = null;
+		dbPass = null;
+	break;
+	case 'production':
+	break;
+	default:
+		if(typeof process.env.NODE_ENV == 'undefined'){
+			mainDomain = "127.0.0.1";
+			mainPort = 8000;
+			dbDomain = 'localhost';
+			dbName = 'buildmypattern_dev';
+			dbUser = null;
+			dbPass = null;
+			process.env.NODE_ENV = 'development'
+		}
+	break;
+}
+console.log('Environment set to:'+process.env.NODE_ENV);
 var app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
+  app.set('port', process.env.PORT || mainPort);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.use(express.favicon());
+  app.use(express.favicon(__dirname + '/public/favicon.ico'));
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
